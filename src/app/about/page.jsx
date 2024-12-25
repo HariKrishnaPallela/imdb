54fe247439fddc9768d8b053ea33cc49b659de96
Hari krishna <pallelaharikrishna85@gmail.com>
2024-12-25 17:14:02 +0530

    Create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+export default function About() {
+    
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <h1>
+        <p>
+          IMDb (Internet Movie Database) is the world’s largest and most
+          comprehensive platform for movie, TV, and celebrity information.
+          Founded in 1990 by Col Needham, IMDb began as a passion project and
+          has grown into a globally recognized resource, now owned by Amazon.
+          With data on over 10 million titles and 11 million industry
+          professionals, it is the go-to destination for fans, cinephiles, and
+          entertainment professionals alike. Whether you’re searching for
+          information on the latest blockbuster, a cult classic, or your
+          favorite actor, IMDb connects users to the stories and creators they
+          love.
+        </p>
+
+        <p>
+          For fans, IMDb offers a wealth of features, including movie and TV
+          show ratings, user reviews, detailed cast and crew information, and
+          curated lists like the IMDb Top 250 Movies. Users can explore
+          trailers, trivia, and box office statistics, creating a fully
+          immersive entertainment experience. For industry professionals,
+          IMDbPro provides exclusive access to tools for networking, production
+          tracking, and career development, making it a vital resource for
+          navigating the entertainment industry. Additionally, IMDb serves as a
+          hub for news and editorial content, keeping users informed about the
+          latest trends and developments.
+        </p>
+
+        <p>
+          With its user-friendly website, mobile apps, and support for multiple
+          languages, IMDb is accessible to audiences worldwide. It provides an
+          engaging platform for discovering, exploring, and discussing
+          entertainment in all its forms. Whether you’re a fan building a
+          watchlist or a professional seeking insights and opportunities, IMDb
+          offers a seamless blend of entertainment discovery and industry
+          expertise, making it an essential resource for anyone passionate about
+          movies and TV.
+        </p>
+      </h1>
+    </div>
+  );
+}
